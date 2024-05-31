12dd24d03a2003a9a729ecfa0150d0228508117a
Александр <fridrix.aleksandr.2004@mail.ru>
2024-05-31 12:53:24 +0300

    done communication through rabbitmq

--- main.go
+++ main.go
@@ -8,26 +8,22 @@ import (
 	"log/slog"
 	"time"
 
-	"github.com/fridrock/tgsportbot/broker"
+	rs "github.com/fridrock/rabbitsimplier"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-// TODO make loose decoupling with .Ch and .Connection properties, so all methods use interfaces instead of implementations
 type CreateExGroupEvent struct {
-	Event  string `json:"event"`
-	Id     int32  `json:"id"`
 	UserId int32  `json:"user_id"`
 	Name   string `json:"name"`
 }
 
 func createExGroup() CreateExGroupEvent {
 	exg := CreateExGroupEvent{}
-	exg.Event = "create"
 	exg.Name = "Back"
 	exg.UserId = 1
 	return exg
 }
-func SendExerciseGroup(br broker.BrokerProducer) {
+func SendExerciseGroup(br rs.Producer) {
 	//creating and marshalling event
 	exg := createExGroup()
 	res, err := json.Marshal(&exg)
@@ -38,24 +34,28 @@ func SendExerciseGroup(br broker.BrokerProducer) {
 	//sending message
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
-	err = br.PublishMessage(ctx, "sport_bot", "trainings.exgroup", string(res))
+	err = br.PublishMessage(ctx, "sport_bot", "trainings.exgroup.create", string(res))
 	if err != nil {
 		slog.Error(fmt.Errorf("error sending message: %v", err).Error())
 	}
 }
 
-func setupConfigurer() *broker.BrokerConfigurerImpl {
-	brc := &broker.BrokerConfigurerImpl{}
-	err := brc.Configure()
+func setupConfigurer() *rs.RConfigurer {
+	brc := &rs.RConfigurer{}
+	err := brc.Configure(rs.Config{
+		Username: "guest",
+		Password: "guest",
+		Host:     "localhost",
+	})
 	if err != nil {
 		log.Fatal("error creating RabbitMQ connection")
 	}
 	return brc
 }
 
-func setupConsumer(brc *broker.BrokerConfigurerImpl) *broker.BrokerConsumerImpl {
-	brConsumer := &broker.BrokerConsumerImpl{}
-	err := brConsumer.CreateChannel(brc.Connection)
+func setupConsumer(brc *rs.RConfigurer) *rs.RConsumer {
+	brConsumer := &rs.RConsumer{}
+	err := brConsumer.CreateChannel(brc.GetConnection())
 	if err != nil {
 		log.Fatal("error creating channel for consumer")
 	}
@@ -63,21 +63,21 @@ func setupConsumer(brc *broker.BrokerConfigurerImpl) *broker.BrokerConsumerImpl 
 	if err != nil {
 		log.Fatal("error creating queue")
 	}
-	err = brConsumer.SetBinding(q, "tgbot.exgroup", "sport_bot")
+	err = brConsumer.SetBinding(q, "tgbot.exgroup.#", "sport_bot")
 	if err != nil {
 		log.Fatal("error creating binding for exgroup queue")
 	}
-	brConsumer.RegisterConsumer(q, func(msgs <-chan amqp.Delivery) {
-		for d := range msgs {
-			fmt.Println(string(d.Body))
-		}
-	})
+	rdispatcher := rs.NewRDispacher()
+	rdispatcher.RegisterHandler("tgbot.exgroup.create", rs.NewHandlerFunc(func(msg amqp.Delivery) {
+		slog.Info(fmt.Sprintf("Exgroup: %s was saved", string(msg.Body)))
+	}))
+	brConsumer.RegisterDispatcher(q, rdispatcher)
 	return brConsumer
 }
 
-func setupProducer(brc *broker.BrokerConfigurerImpl) *broker.BrokerProducerImpl {
-	brp := &broker.BrokerProducerImpl{}
-	err := brp.CreateChannel(brc.Connection)
+func setupProducer(brc rs.Configurer) *rs.RProducer {
+	brp := &rs.RProducer{}
+	err := brp.CreateChannel(brc.GetConnection())
 	if err != nil {
 		log.Fatal("error creating connection for producer")
 	}
@@ -92,12 +92,12 @@ func main() {
 	//setting up connection
 	brc := setupConfigurer()
 	defer brc.Stop()
-	brConsumer := setupConsumer(brc)
-	defer brConsumer.Stop()
-	//setting up producer
 	brp := setupProducer(brc)
 	defer brp.Stop()
 
+	brConsumer := setupConsumer(brc)
+	defer brConsumer.Stop()
+	//setting up producer
 	SendExerciseGroup(brp)
 
 	//for endless program listening
